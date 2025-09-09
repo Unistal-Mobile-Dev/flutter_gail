ac877207581199d6a1e9323e9bf83f101a6b617b
Neeraj Kumar <neerajk@unistal.com>
2025-09-09 14:48:25 +0530

    code modification

--- lib/environmentFlavours/main_gail_prod.dart
+++ lib/environmentFlavours/main_gail_prod.dart
@@ -1,3 +1,4 @@
+import 'package:arcgis_maps/arcgis_maps.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gail/ExportFile/app_export_file.dart';
@@ -5,20 +6,18 @@ import 'package:flutter_gail/root.dart';
 import 'package:flutter_gail/services/firebase/notification_service.dart';
 import 'package:flutter_gail/utils/res/environment_config.dart';
 
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true; // Accept all certs
-  }
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FirebaseService.instance.initializeService();
-  HttpOverrides.global = MyHttpOverrides();
-  // AppColor(themeColor: 0xFF1269AC, themeLightColor: 0xFF278AD7);
+
+    var apiKey = const String.fromEnvironment('API_KEY');
+    if (apiKey.isEmpty) {
+      throw Exception('apiKey undefined');
+    } else {
+      ArcGISEnvironment.apiKey = apiKey;
+    }
+
   var configuredApp = const EnvironmentConfig(
       flavours: EnvironmentFlavours.productionIglCng,
       child: Root(
