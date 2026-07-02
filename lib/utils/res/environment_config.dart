e7ecb5fcc569cd92bc54adc07f45407ac5dc7944
Neeraj Kumar <neerajk@unistal.com>
2026-07-02 14:30:11 +0530

    code modification

--- lib/utils/res/environment_config.dart
+++ lib/utils/res/environment_config.dart
@@ -22,7 +22,7 @@ class EnvironmentConfig extends InheritedWidget {
   String get generalUrlBaseOnFlavour {
     switch (flavours) {
       case EnvironmentFlavours.productionIglCng:
-        return "https://tpimso.smartgasnet.com/";
+        return "https://oil-pims-prod.oilindia.in/";
       case EnvironmentFlavours.developmentIglCng:
         return "https://tpimsdev.smartgasnet.com/";
     }
